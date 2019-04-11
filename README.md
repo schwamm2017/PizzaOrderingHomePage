522223968ec3413a9c952d12fdbbbec0dee7e6a9
Yewei Chen <schwamm2014@outlook.com>
2019-04-12 09:18:51 +1000

    HomePage - HTML and CSS

---
+++ README.md
@@ -0,0 +1,2 @@
+# pizza-order-system-demo
+This is the version 1 of pizza order system
---
+++ index.js
@@ -0,0 +1,16 @@
+
+var acc = document.getElementsByClassName("accordion");
+var accIcon = document.getElementsByClassName("change-to-minus")[0];
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
